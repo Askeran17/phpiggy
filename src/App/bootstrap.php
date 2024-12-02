ed7d85defbcc855211cba9fc1fd2a00f1519f8c0
Mikhail <m.ataev@mail.ru>
2024-12-02 22:29:56 +0100

    Updated

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 require __DIR__ . "/../../vendor/autoload.php";
 
+// Подключите файл, содержащий функцию registerRoutes
+require __DIR__ . "/Config/routes.php"; // Убедитесь, что путь правильный
+
 use Framework\App;
+use function App\Config\registerRoutes;
 
 $app = new App();
+
+registerRoutes($app);
 
 return $app;
